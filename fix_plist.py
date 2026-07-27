5443dd3f678a3e40b3f56b587777e3a455dab204
Revora <98613769+Revora1@users.noreply.github.com>
2026-07-27 17:08:43 +0100

    chore: update Info.plist permissions
    Add microphone access description and clean up redundant plist entries via helper scripts.

--- fix_plist.py
+++ fix_plist.py
@@ -3,11 +3,8 @@ import re
 with open('ios/App/App/Info.plist', 'r') as f:
     content = f.read()
 
-if 'NSMicrophoneUsageDescription' not in content:
-    new_keys = """	<key>NSCameraUsageDescription</key>
+new_keys = """	<key>NSCameraUsageDescription</key>
 	<string>RevitUp needs camera access so you can take photos of your cars and share them with the community.</string>
-	<key>NSMicrophoneUsageDescription</key>
-	<string>RevitUp needs microphone access to record audio when you capture videos of your cars.</string>
 	<key>NSPhotoLibraryUsageDescription</key>
 	<string>RevitUp needs access to your photo library to let you upload photos and videos of your vehicles.</string>
 	<key>NSPhotoLibraryAddUsageDescription</key>
@@ -16,10 +13,9 @@ if 'NSMicrophoneUsageDescription' not in content:
 	<string>RevitUp uses this to deliver personalized ads and content to improve your experience.</string>
 </dict>
 </plist>"""
-    
-    content = content.replace("</dict>\n</plist>", new_keys)
-    
-    with open('ios/App/App/Info.plist', 'w') as f:
-        f.write(content)
 
-print("Info.plist updated safely!")
+content = content.replace("</dict>\n</plist>", new_keys)
+
+with open('ios/App/App/Info.plist', 'w') as f:
+    f.write(content)
+
