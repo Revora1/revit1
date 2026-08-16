1a72fac94c163aff6b4b592ac2821eb5cde5fe45
Tony <tonyang1155@hotmail.co.uk>
2026-08-16 22:13:28 +0100

    Replace corrupted screens folder with clean version

--- mobile-app/screens/src/components/AdMobOverlays.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// Other AdMob overlays have been retired in favor of native feed ads.
-export function AdMobOverlays() {
-  return null;
-}
--- mobile-app/screens/src/components/NativeAd.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const NativeAd: React.FC<{ adUnitId?: string; estimatedHeight?: number }> = () => {
-  return null;
-};
